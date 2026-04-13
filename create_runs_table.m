6e966968126660894eb2df55a7c1c844bce7de49
amsmeier <amsmeier@users.noreply.github.com>
2026-04-12 20:35:09 -0400

    .

--- create_runs_table.m
+++ create_runs_table.m
@@ -1,3 +1,14 @@
+
+%% NB - we need to change this process.... instead of creating this table, we should ....
+.... get the full 'steps' table from Zeyang at the start of the session - have zeyang add a 'run' column to it
+
+%%%%% this script could then be run afterward to merge what AM has added during the run to ZY's notes to create a finalized table
+
+
+
+
+
+
 % create table listing the runs containing usable experimental data (1 run per ses/task)
 %%% this script just creates a table with a blank column for run which must be manually filled out afteward
 %
@@ -5,18 +16,13 @@
 %%% .... and load it at each run to apply the correct step ID
 %%% .... in this case, also fill in run ID during the session
 
-%% SEE IF WE CAN GET STEP-TASK PAIR TABLE FROM ZEYANG AT THE START OF THE SESSION, AND USE IT TO ADD STEP ID TO ALL RAW FILES
-% if so, this script may only need to make minor edits to that table 
-%
-%  .... this script could be automatically run at the beginning of run_exp if the file doesn't exist yet
-
 %%% alternatively, if runs table was not created before/during acquisition, run afterward
 %%% ... in this case, step ID will already have been assigned by some other means
 %%% ... and it can be read from a trials table file and automatically added to the table
 
-function create_runs_table (op)
+function [paths] = create_runs_table (op)
 
-field_default(op,'sub','qqq')
+field_default('op','sub','qqq');
 
 % either run on the specified session or (default) on both sessions
 if isfield(op,'ses')
@@ -29,8 +35,13 @@ for thisses = seslist
     op.ses = thisses{1};
     [paths] = setpaths_dbs_learn(op);
     paths.src_runs_table = [paths.src_ses, filesep, 'sub-',op.sub,'_ses-',op.ses, '_runs.tsv']; 
+
+    if ~exist(paths.src_ses,'dir')
+        mkdir(paths.src_ses)
+    end
+
     if exist(paths.src_runs_table, 'file')
-        fprintf(['Runs table file for ses==',op.ses ,'already exists - skipping \n..........',paths.src_runs_table])
+        fprintf(['\nRuns table file for ses==',op.ses ,' already exists - skipping \n..........%s'],paths.src_runs_table)
     elseif ~exist(paths.landmarks_file,'file')
 
         switch op.ses
@@ -61,8 +72,8 @@ for thisses = seslist
                 runs.step{itask} = steplabel{1}; 
             end
     
+            fprintf(['Creating empty runs table to fill in here:\n    ',paths.src_runs_table,'\n'])
             writetable(runs, paths.src_runs_table, "FileType","text", "Delimiter","tab"); 
-
         end
     end
 
