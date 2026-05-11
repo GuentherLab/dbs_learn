e9df9153b2e1259b97d51a176ab44473d9bf38fe
amsmeier <amsmeier@users.noreply.github.com>
2026-05-11 14:31:48 -0400

    sync table script

--- analysis/create_sync_landmark_tables.m
+++ analysis/create_sync_landmark_tables.m
@@ -6,7 +6,7 @@
 function create_sync_landmark_tables(op)
 
 vardefault('op',struct);
-field_default('op','sub','sml001');
+field_default('op','sub','sml003');
 
 field_default('op','ses','subsyl');
     % field_default('op','ses','multisyl');
@@ -14,7 +14,13 @@ field_default('op','ses','subsyl');
 [paths, compname] = setpaths_dbs_learn(op);
 
 runs = readtable(paths.src_runs_table,'Filetype','text', 'Delimiter','tab'); 
-tasks = runs.task; 
+
+switch op.ses
+    case 'subsyl'
+        tasks = {'famil';'pretest';'trainA';'trainB';'test1';'test2'}; 
+    case 'multisyl'
+        tasks = {'fds';'famil';'assess';'pretest';'trainA';'trainB';'test1';'test2'}; 
+end
 
 filetypes_suffix = {'trials.tsv','trials.tsv';...
     'audio.wav','recording-headphone.wav';...
@@ -26,9 +32,9 @@ nruns = length(tasks);
 nrows = nruns * nfiletypes;
 nancol = nan(nrows,1);
 celcol = cell(nrows,1); 
-runs_col = repelem(runs.run,nfiletypes,1);
+runs_col =  nan(nrows,1);
 tasks_col = repelem(tasks,nfiletypes,1);
-step_col = repelem(runs.step,nfiletypes,1);
+step_col =  nan(nrows,1);
 filetype_col = repmat(filetypes_suffix(:,1),nruns,1);
 suffix_col = repmat(filetypes_suffix(:,2),nruns,1);
 
@@ -38,32 +44,33 @@ sync.t1_description = repmat({'first go beep onset'},nrows,1);
 sync.t2_description = repmat({'last go beep onset'},nrows,1);
 
 
-for irow = 1:nrows
-    op.task = sync.task{irow};
-    op.run = sync.run(irow); 
-    runrowmatch = string(op.task) == runs.task & op.run == runs.run;
+for isynctabrow = 1:nrows
+    op.task = sync.task{isynctabrow};
+    % % % % % op.run = sync.run(isynctabrow); 
+    runrowmatch = string(op.task) == runs.task %%%%% & op.run == runs.run;
 
     if  ismember('step', sync.Properties.VariableNames) && iscell(sync.step) % in subject sml001, we didn't add 'step' to sourcedata filenames
         op.step = runs.step{runrowmatch};
     end
 
+    op.run = runs.run(runrowmatch); 
     paths = setpaths_dbs_learn(op); % get paths.filestr
 
-    switch sync.filetype{irow} % set dir 
+    switch sync.filetype{isynctabrow} % set dir 
         case 'trials.tsv'
-            sync.dir{irow} = paths.beh;
+            sync.dir{isynctabrow} = paths.beh;
 
             if  ismember('step', sync.Properties.VariableNames) && iscell(sync.step) % in subject sml001, we didn't add 'step' to sourcedata filenames            
-                    sync.filename{irow} = [paths.filestr_step, 'trials.tsv'];
+                    sync.filename{isynctabrow} = [paths.filestr_step, 'trials.tsv'];
             else
-                sync.filename{irow} = [paths.filestr, 'trials.tsv'];
+                sync.filename{isynctabrow} = [paths.filestr, 'trials.tsv'];
             end
 
-            sync.t1_description{irow} = 'trials.t_go_aud_on(1)..... first trial go beep onset'; 
+            sync.t1_description{isynctabrow} = 'trials.t_go_aud_on(1)..... first trial go beep onset'; 
 
-            % open trial table to add go beep datapoints
-            trials = readtable([sync.dir{irow}, filesep, sync.filename{irow}], 'Filetype','text', 'Delimiter','tab');
-            sync.t1(irow) = trials.t_go_aud_on(1);
+            % open trial table to add GO beep datapoints
+            trials = readtable([sync.dir{isynctabrow}, filesep, sync.filename{isynctabrow}], 'Filetype','text', 'Delimiter','tab');
+            sync.t1(isynctabrow) = trials.t_go_aud_on(1);
 
             %% trial boundary adjustments
             % %    create boundary adjustments trial file to fill in if it doesn't exist yet
@@ -85,16 +92,16 @@ for irow = 1:nrows
             switch op.task
                 case 'fds'
                     mintrials_fds = 3; 
-                    sync.t2_description{irow} = ['trials.t_go_aud_on(',num2str(mintrials_fds),')..... trial ',num2str(mintrials_fds),' go beep onset']; 
-                    sync.t2(irow) = trials.t_go_aud_on(mintrials_fds); 
+                    sync.t2_description{isynctabrow} = ['trials.t_go_aud_on(',num2str(mintrials_fds),')..... trial ',num2str(mintrials_fds),' go beep onset']; 
+                    sync.t2(isynctabrow) = trials.t_go_aud_on(mintrials_fds); 
                 otherwise
-                    sync.t2_description{irow} = 'trials.t_go_aud_on(end)..... last trial go beep onset'; 
-                    sync.t2(irow) = trials.t_go_aud_on(end); 
+                    sync.t2_description{isynctabrow} = 'trials.t_go_aud_on(end)..... last trial go beep onset'; 
+                    sync.t2(isynctabrow) = trials.t_go_aud_on(end); 
             end
 
         case 'audio.wav'
-            sync.dir{irow} = paths.src_audvid; 
-            sync.filename{irow} = [paths.filestr,'recording-headphone.wav'];
+            sync.dir{isynctabrow} = paths.src_audvid; 
+            sync.filename{isynctabrow} = [paths.filestr,'recording-headphone.wav'];
         case 'preproc.mat'
             % need to specify name 
 
