fbd519d55a13ef14dff39950b7f1c384dc773dcd
amsmeier <amsmeier@users.noreply.github.com>
2026-05-28 08:31:54 -0400

    added toplevel example scripts

--- toplevel_dbs_learn_run_exp_sub_example_subsyl.m
+++ toplevel_dbs_learn_run_exp_sub_example_subsyl.m
@@ -1,30 +1,16 @@
 
 
 op.sub = 'qqq';
-op.ses = 'multisyl'; 
-op.is_dbs_run = 0; 
-op.record_audio = 0; 
-
-%% fds
-op.task='fds'; 
-op.dbs_state = 'on';
-op.step = 'K401';
-dbs_learn_run_exp(op)
+op.ses = 'subsyl'; 
+op.is_dbs_run = 1; 
+op.record_audio = 1; 
+op.syl_struct = 'ccvcc';
 
 %% famil
 op.task='famil'; 
 op.dbs_state = 'on';
 op.step = 'A101';
 dbs_learn_run_exp(op)
-
-%% asses
-op.task='asses'; 
-op.dbs_state = 'on';
-op.step = 'A201';
-dbs_learn_run_exp(op)
-
-%% set number of syls
-op.subj_n_syls = 0;
 
 %% pretest
 op.task='pretest'; 
@@ -38,59 +24,65 @@ op.dbs_state = 'on';
 op.step = 'A401';
 dbs_learn_run_exp(op)
 
-%====================================== Washout_01
+%====================================== 
+%% Washout-1
 %% trainA
 op.task='trainA'; 
-op.dbs_state = 'on';
+op.dbs_state = 'off';
 op.step = 'B201';
 dbs_learn_run_exp(op)
 
-%% ctr base
+%% ctr base - Percept survey mode
 op.task='base-controlled'; 
-op.dbs_state = 'on';
+op.dbs_state = 'off';
 op.step = 'B301';
 dbs_learn_run_exp(op)
 
-%====================================== washout_02 starts
-
-%% ctr base
+%% ctr base - Percept sense mode
 op.task='base-controlled'; 
 op.dbs_state = 'off';
-op.step = 'C401';
+op.step = 'B401';
 dbs_learn_run_exp(op)
+%====================================== washout_02 starts
 
 %% trainB
 op.task='trainB'; 
-op.dbs_state = 'off';
+op.dbs_state = 'on';
 op.step = 'C201';
 dbs_learn_run_exp(op)
 
 %% ctr base
 op.task='base-controlled'; 
-op.dbs_state = 'off';
+op.dbs_state = 'on';
 op.step = 'C301';
 dbs_learn_run_exp(op)
 
+%====================================== washout03 starts
 
 
-%====================================== washout_03 starts
 
 %% test1
 op.task='test1'; 
-op.dbs_state = 'on';
+op.dbs_state = 'off';
 op.step = 'D201';
 dbs_learn_run_exp(op)
 
 %% ctr base
 op.task='base-controlled'; 
-op.dbs_state = 'on';
+op.dbs_state = 'off';
 op.step = 'D301';
 dbs_learn_run_exp(op)
 
-%====================================== washout_04 starts
+%% ctr base
+op.task='base-controlled'; 
+op.dbs_state = 'off';
+op.step = 'D401';
+dbs_learn_run_exp(op)
+
+%====================================== washout04 starts
 %% test2
 op.task='test2'; 
-op.dbs_state = 'off';
+op.dbs_state = 'on';
 op.step = 'E201';
 dbs_learn_run_exp(op)
 
@@ -99,3 +91,5 @@ op.task='base-controlled';
 op.dbs_state = 'off';
 op.step = 'E301';
 dbs_learn_run_exp(op)
+
+
